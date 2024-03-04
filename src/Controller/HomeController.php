9616eaaf43fe34394485b17cae2419fc4480e30e
gwen <gwendoline.garlet@live.fr>
2024-03-04 13:15:40 +0100

    Last commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,32 +14,32 @@ use Knp\Component\Pager\PaginatorInterface;
 
 class HomeController extends AbstractController
 {
+    // Création de la route pour afficher tous les articles
     #[Route('/', name: 'app_home')]
     public function index(HttpClientInterface $articleClient, PaginatorInterface $paginator): Response
     {
-
-
-
-        $response = $articleClient->request(Request::METHOD_POST, '/api/article/');
+        // Effectue une requête HTTP GET vers l'API pour récupérer la liste des articles
+        $response = $articleClient->request(Request::METHOD_GET, '/api/article/');
         // dump($response);
         try {
+            // Transforme la réponse en tableau associatif contenant les articles
             $articles = $response->toArray();
         } catch (ClientExceptionInterface $th) {
+            // En cas d'erreur, capture l'exception et stocke le message d'erreur
             $error = $th->getMessage();
             exit;
         }
-
+         // Rend la vue 'home/index.html.twig' en passant la liste des articles
         return $this->render('home/index.html.twig', [
-            'articles' => $articles['item'],
-            'pagination'      => $articles
-            // 'error'     => $error
+            'articles' => $articles,
         ]);
     }
 
-
+    // Création de la route pour afficher les détails d'un article
     #[Route('/article/{id}', name: 'app_article')]
     public function item(Request $request, HttpClientInterface $articleClient, $id): Response
     {
+        // Effectue une requête HTTP GET vers l'API pour récupérer les détails d'un article spécifique
         $req = $articleClient->request(Request::METHOD_GET, '/api/article/{id}', [
             'json'  => ['id' => $id]
         ]);
